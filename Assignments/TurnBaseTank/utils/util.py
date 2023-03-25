faf30813674b0a6ad6c420dd3611214b8ca805b7
Loic <konanloic10@gmail.com>
2023-03-25 17:57:04 -0500

    README.md

--- Assignments/TurnBaseTank/utils/util.py
+++ Assignments/TurnBaseTank/utils/util.py
@@ -1,17 +1,15 @@
 import pygame
 import math
 from pygame.locals import *
-
 from pygame import Vector2
-
 from utils.camera import Camera
 
 
 class Utils():
-
+    
+    # Initialize the Utils class and set some default values
     def __init__(self):
         pygame.init()
-
         self.height = 720
         self.width = 1280
         self.gameOver = False
@@ -19,20 +17,21 @@ class Utils():
         self.screen = pygame.display.set_mode((self.width, self.height),DOUBLEBUF,16)
         self.dt = 0
         self.clock = pygame.time.Clock()
-
         self.fps = 0
         self.fpsCounter = 0
         self.fpsTimeCount = 0
-
         self.camera = Camera()
 
-    def initDeltaTime(self):  # calculate deltaTime
+    # Method to calculate deltaTime
+    def initDeltaTime(self):  
         t = self.clock.tick(60)
         self.dt = t / 1000
 
+    # Method to return deltaTime
     def deltaTime(self):
         return self.dt
 
+    # Method to show frames per second (fps) on the screen
     def showFps(self):
         self.fpsTimeCount += self.deltaTime()
         self.fpsCounter += 1
@@ -46,15 +45,18 @@ class Utils():
         else:
             self.drawText(Vector2(0, 0), "fps: " + str(self.fps), (233, 23, 23), 16)
 
+    # Method to draw text on the screen
     def drawText(self, pos, text, color, size):  # draw text
         self.font = pygame.font.Font('assets/Merchant.ttf', size)
         text = self.font.render(text, True, color)
         self.screen.blit(text, (pos.x, pos.y))
 
+    # Method to calculate distance between two points
     def distance(self, x1, y1, x2, y2):
         return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) * 1.0);
 
-    def collide(self, a, b):  # aabb 2 box collide check
+    # Method to check for AABB collision between two boxes
+    def collide(self, a, b):  
         rect = a.getRect()
         r = b.getRect()
         return (
@@ -64,11 +66,13 @@ class Utils():
             and r.h + r.y > rect.y
         )
 
+
+    # Method to rotate a surface around a pivot point
     def rotate(self, surface, angle, pivot, offset):
         rotated_image = pygame.transform.rotozoom(surface, -angle, 1)
         rotated_offset = offset.rotate(angle)
         rect = rotated_image.get_rect(center=pivot + rotated_offset)
         return rotated_image, rect
 
-
-utils = Utils()  # util is global object
+# Global Utils object
+utils = Utils() 
