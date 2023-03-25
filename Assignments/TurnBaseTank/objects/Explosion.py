1c38dcb08947a69e2867222ab7a14359ddea5522
Loic <konanloic10@gmail.com>
2023-03-25 14:56:36 -0500

    README.md

--- Assignments/TurnBaseTank/objects/Explosion.py
+++ Assignments/TurnBaseTank/objects/Explosion.py
@@ -9,6 +9,19 @@ from utils.assets_manager import assetsManager
 from utils.util import utils
 
 
+##############################################################################################
+# class Explosion(GameObject):
+#
+#   - This program defines a class Explosion that extends from GameObject. 
+#   - The  __init__ method that initializes an animation sprite sheet from an image file, 
+#   - sets the play range and animation speed, and then calls the superclass constructor 
+#   - with the initial position, the current animation frame, and a name.
+#   - The update and draw methods that respectively update the animation state 
+#   - and render the current frame. The update method calls the superclass update method 
+#   - and then updates the animation state with the play method.
+
+##############################################################################################
+
 class Explosion(GameObject):
     def __init__(self, pos):
         self.animSheet = SpriteSheet(assetsManager.get("explo1"), 1, 12)
