401a50711d290e130f5d1788a4731c11879d7766
Loic <konanloic10@gmail.com>
2023-03-25 15:15:26 -0500

    README.md

--- Assignments/TurnBaseTank/main.py
+++ Assignments/TurnBaseTank/main.py
@@ -2,27 +2,43 @@ import pygame
 from game import Game
 from utils.util import utils
 
+# Create a new Game instance
 game = Game()
 
+# Game loop
 while True:
+    # Set the background color of the screen to dark green
     utils.screen.fill((22, 122, 122), (0, 0, utils.width, utils.height))
+    
+    # Initialize the delta time used for animation and movement updates
     utils.initDeltaTime()
 
+    # Process all events in the Pygame event queue
     for event in pygame.event.get():
+        # If the user closes the window, exit the program
         if event.type == pygame.QUIT:
             exit(0)
+        # If a key is pressed, call the onKeyDown method in the Game instance with the key as an argument
         if event.type == pygame.KEYDOWN:
             game.onKeyDown(event.key)
+        # If a key is released, call the onKeyUp method in the Game instance with the key as an argument
         if event.type == pygame.KEYUP:
             game.onKeyUp(event.key)
+        # If the mouse button is pressed, call the onMouseDown method in the Game instance with the event as an argument
         if event.type == pygame.MOUSEBUTTONDOWN:
             game.onMouseDown(event)
+        # If the mouse button is released, call the onMouseUp method in the Game instance with the event as an argument
         if event.type == pygame.MOUSEBUTTONUP:
             game.onMouseUp(event)
 
+    # Update the Game instance with the current delta time
     game.update()
+
+    # Draw the current frame using the Game instance
     game.draw()
 
+    # Display the current frame rate on the screen
     utils.showFps()
 
+    # Update the screen with the new frame
     pygame.display.flip()
--- Assignments/TurnBaseTank/objects/Explosion.py
+++ Assignments/TurnBaseTank/objects/Explosion.py
@@ -1,5 +1,4 @@
 import math
-
 import pygame
 from pygame import Vector2
 
@@ -8,30 +7,30 @@ from utils.SpriteSheet import SpriteSheet
 from utils.assets_manager import assetsManager
 from utils.util import utils
 
-
-##############################################################################################
-# class Explosion(GameObject):
-#
-#   - This program defines a class Explosion that extends from GameObject. 
-#   - The  __init__ method that initializes an animation sprite sheet from an image file, 
-#   - sets the play range and animation speed, and then calls the superclass constructor 
-#   - with the initial position, the current animation frame, and a name.
-#   - The update and draw methods that respectively update the animation state 
-#   - and render the current frame. The update method calls the superclass update method 
-#   - and then updates the animation state with the play method.
-
-##############################################################################################
-
 class Explosion(GameObject):
     def __init__(self, pos):
+        # Create a new SpriteSheet instance with the explosion animation image,
+        # specifying the number of rows and columns in the image
         self.animSheet = SpriteSheet(assetsManager.get("explo1"), 1, 12)
+        
+        # Set the range of frames to play for the animation,
+        # specifying the start frame index, end frame index, frame duration, and whether the animation should loop
         self.animSheet.setPlay(0, 11, 0.1, False)
+        
+        # Call the __init__ method of the superclass (GameObject) with the initial position,
+        # the current frame of the animation sheet, and the name of the object
         super().__init__(pos, self.animSheet.getCurrentFrame(), "explo")
 
     def update(self):
+        # Call the update method of the superclass (GameObject) to update the position and velocity
         super().update()
+        
+        # Play the animation by calling the play method of the animation sheet
         self.animSheet.play()
 
     def draw(self):
+        # Set the current frame of the animation sheet as the image to draw
         self.img = self.animSheet.getCurrentFrame()
+        
+        # Call the draw method of the superclass (GameObject) to draw the image at the current position
         super().draw()
